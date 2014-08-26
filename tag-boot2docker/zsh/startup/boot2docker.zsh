b07e9203924e75d6f3ca7fea9768fd003da0692b
Christian Höltje <choltje@us.ibm.com>
2014-08-26 14:14:40 -0400

    boot2docker: port is incorrectly reported as 0
    Due to a [bug in
    boot2docker](https://github.com/boot2docker/boot2docker/issues/504) the
    `DockerPort` was being reported as `0`.  This is a workaround.

--- tag-boot2docker/zsh/startup/boot2docker.zsh
+++ tag-boot2docker/zsh/startup/boot2docker.zsh
@@ -5,6 +5,10 @@ function boot2docker-fixup
   sudo -v
   ip=$(boot2docker ip 2>/dev/null)
   port=$(boot2docker config 2>/dev/null | perl -n -e 'print $1 if m/^DockerPort\s*=\s*(\d+)$/;')
+  if [ "${port}" = "0" ]; then
+    # https://github.com/boot2docker/boot2docker/issues/504
+    port=2375
+  fi
 
   export DOCKER_HOST="tcp://${ip}:${port}"
   echo "export DOCKER_HOST=${DOCKER_HOST}" > ~/.zsh/startup/boot2docker.zshenv
