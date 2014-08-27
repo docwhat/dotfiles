eac5441fcceae02a7faf20a584bebd02b0a25199
Christian Höltje <choltje@us.ibm.com>
2014-08-27 11:13:58 -0400

    made boot2docker-fixup be less verbose

--- tag-boot2docker/zsh/startup/boot2docker-fixup.sh
+++ tag-boot2docker/zsh/startup/boot2docker-fixup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
-# Fix NTP/Time
-# https://github.com/boot2docker/boot2docker/issues/290
-boot2docker ssh -- sudo killall -9 ntpd ntpclient || :
-boot2docker ssh -- sudo ntpclient -s -h pool.ntp.org
+echo 'Fix the time by running NTP.'
+echo '    https://github.com/boot2docker/boot2docker/issues/290'
+boot2docker ssh -- sudo killall -9 ntpd ntpclient '2>/dev/null' '||' :
+boot2docker ssh -- sudo ntpclient -s -h pool.ntp.org '>/dev/null'
 boot2docker ssh -- sudo ntpd -p pool.ntp.org
 
-# Set the TZ
-# https://github.com/boot2docker/boot2docker/issues/476
-scp -i ~/.ssh/id_boot2docker -P 2022 /etc/localtime docker@localhost:/tmp/
+echo 'Set the time zone.'
+echo '    https://github.com/boot2docker/boot2docker/issues/476'
+scp -qi ~/.ssh/id_boot2docker -P 2022 /etc/localtime docker@localhost:/tmp/
 boot2docker ssh -- sudo cp /tmp/localtime /etc/localtime
 
-# Create /data used for data-only containers and such
+
+echo 'Create /data used for volume data.'
 boot2docker ssh -- sudo mkdir -p /data
 boot2docker ssh -- sudo chown 1000:1000 /data
 
