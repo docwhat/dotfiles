c575d6802c15e81f5fdddfba4cb9b2aae7a06bc9
Christian Höltje <choltje@us.ibm.com>
2014-08-27 11:06:17 -0400

    boot2docker: use shellinit instead of guessing

--- tag-boot2docker/zsh/startup/boot2docker.zsh
+++ tag-boot2docker/zsh/startup/boot2docker.zsh
@@ -3,15 +3,11 @@
 function boot2docker-fixup
 {
   sudo -v
-  ip=$(boot2docker ip 2>/dev/null)
-  port=$(boot2docker config 2>/dev/null | perl -n -e 'print $1 if m/^DockerPort\s*=\s*(\d+)$/;')
-  if [ "${port}" = "0" ]; then
-    # https://github.com/boot2docker/boot2docker/issues/504
-    port=2375
-  fi
+  local ip=$(boot2docker ip 2>/dev/null)
+  local docker_env="$(boot2docker shellinit)"
 
-  export DOCKER_HOST="tcp://${ip}:${port}"
-  echo "export DOCKER_HOST=${DOCKER_HOST}" > ~/.zsh/startup/boot2docker.zshenv
+  eval "${docker_env}"
+  echo "${docker_env}" > ~/.zsh/startup/boot2docker.zshenv
 
   sudo -H ruby ~/.zsh/startup/boot2docker-host.rb "${ip}"
 
