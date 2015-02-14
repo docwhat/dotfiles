fe366ccceecf745c653b25e313a3b66d3e236ed5
Christian Höltje <docwhat@gerf.org>
2015-02-14 00:12:17 -0500

    boot2docker: removed NTP hack
    boot2docker/boot2docker#290 is now fixed.
    
    Closed by boot2docker/boot2docker#661

--- tag-boot2docker/zsh/startup/boot2docker-fixup.sh
+++ tag-boot2docker/zsh/startup/boot2docker-fixup.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-echo 'Fix the time by running NTP.'
-echo '    https://github.com/boot2docker/boot2docker/issues/290'
-boot2docker ssh -- sudo killall -9 ntpd ntpclient '2>/dev/null' '||' :
-boot2docker ssh -- sudo ntpclient -s -h pool.ntp.org '>/dev/null'
-boot2docker ssh -- sudo ntpd -p pool.ntp.org
-
 echo 'Set the time zone.'
 echo '    https://github.com/boot2docker/boot2docker/issues/476'
 scp -qi ~/.ssh/id_boot2docker -P 2022 /etc/localtime docker@localhost:/tmp/
