7bc608669e8b033af074c0fffba9829f1ebfd800
Christian Höltje <choltje@us.ibm.com>
2015-05-07 11:15:59 -0400

    fix term on kitchen login

--- zsh/startup/chef.zsh
+++ zsh/startup/chef.zsh
@@ -4,5 +4,19 @@ if [[ -d /opt/chef || -d /opt/chefdk || -d ~/.chef ]]; then
   alias kv="kitchen verify"
   alias kd="kitchen destroy"
   alias kt="kitchen test"
-  alias ks="kitchen login"
+  function ks
+  {
+    case "$TERM" in
+      screen*)
+        new_term=screen
+        ;;
+      xterm*)
+        new_term=xterm
+        ;;
+      *)
+        new_term=$TERM
+    esac
+
+    env TERM="${new_term}" kitchen login "$@"
+  }
 fi
