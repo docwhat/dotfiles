c4ec9ba2c1cdcb020ec52974e065c8554ba9d36e
Christian Höltje <choltje@us.ibm.com>
2015-06-08 16:11:10 -0400

    fixed sh-isms

--- bin/rubocop.sh
+++ bin/rubocop.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
-if [ $# -eq 1 -a x"$1" = x--version ]; then
+# Work around for a bug in rubocop, ruby-parser and ruby:
+# https://github.com/scrooloose/syntastic/issues/1418
+if [ $# -eq 1 -a "${1:-}" = --version ]; then
     rubocop "$@" 2>&1 | \
         sed '/warning: parser\/current is loading parser\/ruby[0-9]*, which recognizes/d;
              /warning: [0-9.]*-compliant syntax, but you are running [0-9.]/d'
