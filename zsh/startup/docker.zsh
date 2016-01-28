061e9f7de7a467adc2d84e0212299941852d28a0
Christian Höltje <choltje@us.ibm.com>
2016-01-28 09:43:16 -0500

    Source the docker-machine env if running

--- zsh/startup/docker.zsh
+++ zsh/startup/docker.zsh
@@ -1,4 +1,12 @@
 # vim: ft=zsh :
 
 ## Ensure docker-compose doesn't get messed up by certificate settings.
-alias docker-compose="env -u REQUESTS_CA_BUNDLE docker-compose"
+if (( $+commands[docker-compose] )); then
+  alias docker-compose="env -u REQUESTS_CA_BUNDLE docker-compose"
+fi
+
+if (( $+commands[docker-machine] )); then
+  if [ -n "$(docker-machine ls --filter='state=running' --filter='name=default' --format='ok' 2>/dev/null)" ]; then
+    eval "$(docker-machine env default </dev/null 2>/dev/null)"
+  fi
+fi
