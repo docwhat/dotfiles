cfc9086bedf6a83eaf6565d0d89408e07ddf2ea4
Christian Höltje <choltje@us.ibm.com>
2016-01-28 13:33:40 -0500

    Fixed lousy variable name

--- zsh/startup/00omz.zsh
+++ zsh/startup/00omz.zsh
@@ -1,11 +1,10 @@
 
-for lousy_aliases in cp rm mv; do
-  if (( $+aliases[$lousy_aliases] )); then
-    unalias $lousy_aliases
+for lousy_alias in cp rm mv; do
+  if (( $+aliases[$lousy_alias] )); then
+    unalias $lousy_alias
   fi
 done
-
-unset lousy_aliases
+unset lousy_alias
 
 # Fix the SHORT_HOST if it has (2) after it.
 if [ -n "${SHORT_HOST}" ]; then
