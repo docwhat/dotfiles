b6cdeb6d3748efbc8284a9965e9d2abb88db6d05
Christian Höltje <docwhat@gerf.org>
2016-02-27 01:35:47 -0500

    Force using ag

--- zsh/startup/ack.zsh
+++ zsh/startup/ack.zsh
@@ -1,7 +1,14 @@
+# vim: ft=zsh :
+
+# While this is named after ack, when I switched to ag (the Silver Searcher) I didn't
+# rename the file.
+
 if (( $+commands[ag] )); then
   alias  ag="ag --pager 'less -RS' --smart-case -C3 --color-line-number '1;34' --color-match '1;04;31' --color-path '1;32'"
-  alias ack="ag --pager 'less -RS' --smart-case -C3 --color-line-number '1;34' --color-match '1;04;31' --color-path '1;32'"
-fi
-if (( $+commands[ack] )); then
-  alias hl="ack --passthru"
+  alias agr='ag --ruby'
+  alias agy='ag --yaml'
+  alias agm='ag --markdown'
+  alias agh='ag --html'
+  alias agx='ag --xml'
+  alias ack="echo 'Use ag instead.' ; false"
 fi
