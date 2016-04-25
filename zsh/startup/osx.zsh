876ccab67a4ad917da339c9824bdcfab4de1bb33
Christian Höltje <choltje@us.ibm.com>
2016-04-25 11:35:19 -0400

    Changed super-tools-update to a function

--- zsh/startup/osx.zsh
+++ zsh/startup/osx.zsh
@@ -79,7 +79,16 @@ if [[ "${OSTYPE}" == darwin* ]]; then
     fi
   }
 
-  alias super-tools-update="tools-update && rbenv-update-plugins && update-gems && (rbenv use system && brew update &&  brew upgrade && brew cleanup) && brew-cask-upgrade && brew cask cleanup && vim -c ':PlugUpgrade' -c ':PlugUpdate' -c ':qa\!'"
+  function super-tools-update() {
+    tools-update &&\
+      rbenv-update-plugins &&\
+      update-gems &&\
+      (rbenv use system && brew update && brew upgrade && brew cleanup) &&\
+      brew-cask-upgrade &&\
+      brew cask cleanup &&\
+      vim -c ':PlugUpgrade' -c ':PlugUpdate' -c ':qa!'
+    return $?
+  }
 
   export BROWSER='open'
 fi
