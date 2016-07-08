bef17e48af9321a90e88d758f899a5871122124f
Christian Höltje <docwhat@gerf.org>
2016-07-07 21:52:40 -0400

    Don't use docker-machine if Docker-for-Mac is installed

--- zsh/startup/docker.zsh
+++ zsh/startup/docker.zsh
@@ -5,7 +5,7 @@ if (( $+commands[docker-compose] )); then
   alias docker-compose="env -u REQUESTS_CA_BUNDLE docker-compose"
 fi
 
-if (( $+commands[docker-machine] )); then
+if [ ! -d /Applications/Docker.app ] && (( $+commands[docker-machine] )); then
   if [ -n "$(docker-machine ls --filter='state=running' --filter='name=default' --format='ok' 2>/dev/null)" ]; then
     eval "$(docker-machine env default </dev/null 2>/dev/null)"
   fi
