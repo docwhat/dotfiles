5102cdb4f3577ad3fd9128b6b638519389d76458
Christian Höltje <docwhat@gerf.org>
2016-07-22 09:37:27 -0400

    wrong if syntax

--- zsh/startup/iterm.zsh
+++ zsh/startup/iterm.zsh
@@ -3,7 +3,7 @@ if [ "$TERM_PROGRAM" = 'iTerm.app' ]; then
     local template="\e]${1}\007"
     shift
 
-    if [[ -n "$TMUX" -o "$TERM" = tmux* ]]; then
+    if [[ -n "$TMUX" || "$TERM" = tmux* ]]; then
       template="\ePtmux;\e${template}\e\\"
     fi
     printf "$template" "$@"
