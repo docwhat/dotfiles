66e68ceafdb80eadcd8ac9df6a0bd41436f8cfdb
Christian Höltje <docwhat@gerf.org>
2016-09-14 21:03:34 -0400

    Fixed ssh function
    It wasn't resetting the naming properties in TMUX correctly.

--- zsh/startup/tmux.zsh
+++ zsh/startup/tmux.zsh
@@ -2,15 +2,13 @@ if [ -n "$TMUX" ]; then
 
   # Alias so ssh will change the window name.
   ssh() {
-    local old_allow_rename="$(tmux showw -g automatic-rename | cut -d ' ' -f 2)"
-    local old_automatic_rename="$(tmux showw -g automatic-rename | cut -d ' ' -f 2)"
     local old_name="$(tmux display-message -p '#{window_name}')"
-    tmux setw automatic-rename off 1>/dev/null
-    tmux setw allow-rename off 1>/dev/null
+    tmux set-window-option automatic-rename off >/dev/null
+    tmux set-window-option allow-rename off >/dev/null
     tmux rename-window "ssh/$*"
     command ssh "$@"
-    tmux setw allow-rename "${old_allow_rename}" 1>/dev/null
-    tmux setw automatic-rename "${old_automatic_rename}" 1>/dev/null
     tmux rename-window "${old_name}"
+    tmux set-window-option -u allow-rename >/dev/null
+    tmux set-window-option -u automatic-rename >/dev/null
   }
 fi
