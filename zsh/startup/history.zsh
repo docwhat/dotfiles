bf056d5860140e0760452c16d77a3baf30e7fa7f
Christian Höltje <docwhat@gerf.org>
2016-09-15 23:41:55 -0400

    Various tweaks to history while debugging other issues

--- zsh/startup/history.zsh
+++ zsh/startup/history.zsh
@@ -5,7 +5,15 @@ HISTFILE=~/.zsh/history
 HISTSIZE=50000
 SAVEHIST=$(( $HISTSIZE - 200 )) # Give space for saving dups
 
-setopt hist_beep        # Beep on history search errors
-setopt no_share_history # Don't automatically import new entries in the histfile. Use rehist instead.
+setopt HIST_BEEP
+setopt EXTENDED_HISTORY
+setopt NO_SHARE_HISTORY
+setopt NO_INC_APPEND_HISTORY
+setopt INC_APPEND_HISTORY_TIME
+setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_DUPS
+setopt HIST_VERIFY
+setopt HIST_FCNTL_LOCK
 
+# Don't automatically import new entries in the histfile. Use rehist instead.
 alias rehist='fc -Ri'
