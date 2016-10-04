67bab7a854ae40e80ecce91f3695fb049926e7f5
Christian Höltje <docwhat@gerf.org>
2016-10-04 01:38:25 -0400

    Added jpm alias for Developer firefox

---
+++ zsh/startup/firefox.zsh
@@ -0,0 +1,5 @@
+# vim: ft=zsh :
+
+if [ -x /Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox ]; then
+  alias jpm="jpm --binary /Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox"
+fi
