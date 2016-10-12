2966b7694e243d1dbd60a498d31d67efaebfb959
root <docwhat@gerf.org>
2016-10-11 23:13:25 -0400

    Moved plugin config into zshenv

--- zsh/startup/0plugins.zsh
+++ zsh/startup/0plugins.zsh
@@ -1,15 +1,3 @@
-# Oh my zsh config
-# See https://github.com/robbyrussell/oh-my-zsh/blob/master/templates/zshrc.zsh-template
-DISABLE_AUTO_UPDATE=true
-DISABLE_LS_COLORS=true
-DISABLE_AUTO_TITLE=true
-ENABLE_CORRECTION=true
-COMPLETION_WAITING_DOTS=true
-HIST_STAMPS=yyyy-mm-dd
-
-# zsh-completion-generator
-GENCOMPL_FPATH="$HOME/.zsh/cache/complete"
-
 source "${HOME}/.zgen/zgen.zsh"
 
 function zsh-startup-zgen()
---
+++ zsh/startup/0plugins.zshenv
@@ -0,0 +1,15 @@
+# Plugin Configuration Variables
+
+# Oh my zsh config
+# See https://github.com/robbyrussell/oh-my-zsh/blob/master/templates/zshrc.zsh-template
+DISABLE_AUTO_UPDATE=true
+DISABLE_LS_COLORS=true
+DISABLE_AUTO_TITLE=true
+ENABLE_CORRECTION=true
+COMPLETION_WAITING_DOTS=true
+HIST_STAMPS=yyyy-mm-dd
+
+# zsh-completion-generator
+GENCOMPL_FPATH="$HOME/.zsh/cache/complete"
+
+# EOF
