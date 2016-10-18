c866ca6aac8212e5970421974ffc5c49ee644d6f
Christian Höltje <docwhat@gerf.org>
2016-10-18 11:42:11 -0400

    fixed ssh wrapper
    * supports returning the exit code correctly.
    * better tmux option handling.

--- zsh/startup/tmux.zsh
+++ zsh/startup/tmux.zsh
@@ -1,14 +1,25 @@
 if [ -n "$TMUX" ]; then
 
+  # Returns the arguments to pass to tmux set-window-option to restore
+  # a setting.
+  get_tmux_window_option() {
+    local arg_name=$1
+    local value="$(tmux show-window-option ${arg_name})"
+    if [ -z "$value" ]; then
+      value="-u ${(q)arg_name}"
+    fi
+    echo "tmux set-window-option ${value}"
+  }
+
   # Alias so ssh will change the window name.
   ssh() {
     local old_name="$(tmux display-message -p '#{window_name}')"
-    tmux set-window-option automatic-rename off >/dev/null
-    tmux set-window-option allow-rename off >/dev/null
+    local reset_eval="$(get_tmux_window_option automatic-rename) ; $(get_tmux_window_option allow-rename)"
     tmux rename-window "ssh/$*"
     command ssh "$@"
+    local ec=$?
     tmux rename-window "${old_name}"
-    tmux set-window-option -u allow-rename >/dev/null
-    tmux set-window-option -u automatic-rename >/dev/null
+    eval "${reset_eval}"
+    return $ec
   }
 fi
