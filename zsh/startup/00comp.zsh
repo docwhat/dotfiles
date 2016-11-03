c70b6eee839002450bd843c3216aa927b4027835
Christian Höltje <docwhat@gerf.org>
2016-11-02 20:53:36 -0400

    sourcing zgen only works if the comp env vars are set

--- zsh/startup/00comp.zsh
+++ zsh/startup/00comp.zsh
@@ -3,11 +3,6 @@ if [ ! -d "${ZDOTDIR:-${HOME}}/.zsh/cache" ]; then
   mkdir -p "${ZDOTDIR:-${HOME}}/.zsh/cache"
 fi
 
-# Save the location of the current completion dump file.
-# Forces oh-my-zsh to use this location.
-ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zsh/cache/zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
-ZGEN_CUSTOM_COMPDUMP=$ZSH_COMPDUMP
-
 # Force starting compinit as early as possible
 autoload -Uz compinit && compinit -d "$ZSH_COMPDUMP"
 
---
+++ zsh/startup/00comp.zshenv
@@ -0,0 +1,6 @@
+# vim: set ft=zsh :
+
+# Save the location of the current completion dump file.
+# Forces oh-my-zsh to use this location.
+ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zsh/cache/zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+ZGEN_CUSTOM_COMPDUMP=$ZSH_COMPDUMP
