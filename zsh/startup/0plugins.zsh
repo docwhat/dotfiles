228d9875809a0662a427cf13e51c6390f897b210
Christian Höltje <docwhat@gerf.org>
2016-11-03 22:45:27 -0400

    Cleaned up my plugins a little.
    I wish zsh-autosuggestions, zsh-syntax-highlighting, and history-incremental-pattern-search-backward worked together

--- zsh/startup/0plugins.zsh
+++ zsh/startup/0plugins.zsh
@@ -1,63 +1,69 @@
+autoload -U is-at-least
 source "${HOME}/.zgen/zgen.zsh"
 
-function zsh-startup-zgen()
-{
-  if ! zgen saved; then
+if ! zgen saved; then
+  zgen oh-my-zsh
 
-    zgen oh-my-zsh
+  # Python
+  zgen oh-my-zsh plugins/python
+  zgen oh-my-zsh plugins/pip
+  zgen load sharat87/pip-app
 
-    # Python
-    zgen oh-my-zsh plugins/python
-    zgen oh-my-zsh plugins/pip
-    zgen load sharat87/pip-app
+  # Ruby
+  zgen load unixorn/rake-completion.zshplugin
 
-    # Ruby
-    zgen load unixorn/rake-completion.zshplugin
+  # Golang
+  zgen oh-my-zsh plugins/golang
 
-    # Golang
-    zgen oh-my-zsh plugins/golang
+  # Various Utils
+  zgen oh-my-zsh plugins/sudo
+  zgen oh-my-zsh plugins/rsync
+  zgen oh-my-zsh plugins/vagrant
 
-    # Various Utils
-    zgen oh-my-zsh plugins/sudo
-    zgen oh-my-zsh plugins/rsync
-    zgen oh-my-zsh plugins/vagrant
+  zgen load zsh-users/zsh-completions src
 
-    zgen load zsh-users/zsh-completions src
+  # Git
+  zgen load unixorn/git-extra-commands
+  zgen oh-my-zsh plugins/git
+  zgen oh-my-zsh plugins/github
 
-    # Git
-    zgen load unixorn/git-extra-commands
-    zgen oh-my-zsh plugins/git
-    zgen oh-my-zsh plugins/github
+  # zgen oh-my-zsh plugins/colored-man-pages
+  #
+  if [ $(uname -a | grep -ci Darwin) = 1 ]; then
+    # Load OSX-specific plugins
+    zgen oh-my-zsh plugins/osx
+  fi
 
-    # zgen oh-my-zsh plugins/colored-man-pages
-    #
-    if [ $(uname -a | grep -ci Darwin) = 1 ]; then
-      # Load OSX-specific plugins
-      zgen oh-my-zsh plugins/osx
-    fi
+  zgen load chrissicool/zsh-256color
 
-    zgen load chrissicool/zsh-256color
+  zgen oh-my-zsh plugins/redis-cli
+  zgen oh-my-zsh plugins/rsync
+  zgen oh-my-zsh plugins/marked2
 
-    zgen oh-my-zsh plugins/redis-cli
-    zgen oh-my-zsh plugins/rsync
-    zgen oh-my-zsh plugins/marked2
+  # Completion generator
+  zgen load RobSis/zsh-completion-generator
 
-    # Completion generator
-    zgen load RobSis/zsh-completion-generator
+  # Add Fish shell like syntax highlighting for ZSH
+  if false; then
+    # With zsh-syntax-highlighting and zsh-autosuggestions both loaded
+    # then history-incremental-pattern-search-backward doesn't work:
+    # it only works for the first (random) few history items, then stops as if
+    # my history is gone.
+    zgen load zsh-users/zsh-syntax-highlighting
+  fi
 
-    # Add Fish-like autosuggestions to your ZSH
+  # Add Fish-like autosuggestions to your ZSH
+  if is-at-least 5.0; then
     zgen load zsh-users/zsh-autosuggestions
     # Fixes paste being slow.
     zstyle ':bracketed-paste-magic' active-widgets '.self-*'
 
     ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=( end-of-line vi-end-of-line vi-add-eol )
     ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=( forward-char vi-forward-char )
-
-    # Save it all to init script
-    zgen save
   fi
-}
 
-zsh-startup-zgen
+  # Save it all to init script
+  zgen save
+fi
 
 # EOF
