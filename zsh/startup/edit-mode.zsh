8bec144efd06c0a48790d7619b5e4a2c30970a71
Christian Höltje <docwhat@gerf.org>
2016-11-03 21:37:04 -0400

    got rid of my partial edit ^R search thingy

--- zsh/startup/edit-mode.zsh
+++ zsh/startup/edit-mode.zsh
@@ -10,18 +10,7 @@ bindkey '^N'  down-history
 bindkey '^W'  backward-kill-word
 
 # I like pattern searches.
-autoload -Uz narrow-to-region
-function _history-incremental-preserving-pattern-search-backward
-{
-  local state
-  MARK=CURSOR  # magick, else multiple ^R don't work
-  narrow-to-region -p "$LBUFFER${BUFFER:+⁍ }" -P "${BUFFER:+ ⁌}$RBUFFER" -S state
-  zle end-of-history
-  zle history-incremental-pattern-search-backward
-  narrow-to-region -R state
-}
-zle -N _history-incremental-preserving-pattern-search-backward
-bindkey "^R" _history-incremental-preserving-pattern-search-backward
+bindkey "^R" history-incremental-pattern-search-backward
 bindkey "^S" history-incremental-pattern-search-forward
 
 bindkey -M isearch "^R" history-incremental-pattern-search-backward
