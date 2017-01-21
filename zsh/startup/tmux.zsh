85a998e42100fa95aee24f0895d1e8cdfcd5d47b
Christian Höltje <docwhat@gerf.org>
2017-01-21 01:39:46 -0500

    fixed ssh-tmux window rename bug

--- zsh/startup/tmux.zsh
+++ zsh/startup/tmux.zsh
@@ -3,22 +3,24 @@ if [ -n "$TMUX" ]; then
   # Returns the arguments to pass to tmux set-window-option to restore
   # a setting.
   get_tmux_window_option() {
-    local arg_name=$1
-    local value="$(tmux show-window-option ${arg_name})"
+    local wid=$1
+    local arg_name=$2
+    local value="$(tmux show-window-option ${(q)arg_name})"
     if [ -z "$value" ]; then
       value="-u ${(q)arg_name}"
     fi
-    echo "tmux set-window-option ${value}"
+    echo "tmux set-window-option -t ${(q)wid} ${value}"
   }
 
   # Alias so ssh will change the window name.
   ssh() {
+    local wid="$(tmux display-message -p '#{window_id}')"
     local old_name="$(tmux display-message -p '#{window_name}')"
-    local reset_eval="$(get_tmux_window_option automatic-rename) ; $(get_tmux_window_option allow-rename)"
-    tmux rename-window "ssh/$*"
+    local reset_eval="$(get_tmux_window_option ${wid} automatic-rename) ; $(get_tmux_window_option ${wid} allow-rename)"
+    tmux rename-window -t "${wid}" "ssh/$*"
     command ssh "$@"
     local ec=$?
-    tmux rename-window "${old_name}"
+    tmux rename-window -t "${wid}" "${old_name}"
     eval "${reset_eval}"
     return $ec
   }
