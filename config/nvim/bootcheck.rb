320d97931743d28a4c907e5abf71879df312b0cf
Christian Höltje <docwhat@gerf.org>
2017-02-03 23:07:14 -0500

    nvim: rewrote bootstrap check
    Cleaned it up to be more ruby-like and it now detects missing plugins,
    too.

--- config/nvim/bootcheck.rb
+++ config/nvim/bootcheck.rb
@@ -1,12 +1,48 @@
 # Checks if the plugin list has changed.  If it has, then
 # force a plugin update.
 
-cache_path = File.expand_path '~/.config/nvim/.plugin.check'
+require 'json'
 
-current_plugins = VIM.evaluate('g:plugs').inspect
-previous_plugins = File.readable?(cache_path) ? File.read(cache_path) : ''
+# Class to hold plugins
+class Plugins
+  def initialize
+    @plugins = VIM.evaluate 'g:plugs'
+  end
 
-if previous_plugins != current_plugins
-  VIM.command 'let s:bootstrap=1'
-  File.write(cache_path, current_plugins)
+  def to_json
+    JSON.pretty_generate(@plugins) + "\n"
+  end
+
+  def data_home
+    @data_home ||= File.expand_path(
+      VIM.evaluate('g:xdg_data_home') || '~/.config/nvim'
+    )
+  end
+
+  def filename
+    @filename ||= File.join(data_home, 'plugins.json')
+  end
+
+  def previous_json
+    @previous_json ||= File.readable?(filename) ? File.read(filename) : ''
+  end
+
+  def changed?
+    return true if previous_json != to_json
+    @plugins.each_pair do |_name, data|
+      return true unless File.directory? data['dir']
+    end
+    false
+  end
+
+  def write!
+    File.write(filename, to_json)
+  end
+end
+
+Plugins.new.tap do |plugins|
+  if plugins.changed?
+    VIM.command 'let s:bootstrap=1'
+    plugins.write!
+  end
 end
