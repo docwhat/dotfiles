5071749cea700bc5a5be1097345e303f67c1c51b
Christian Höltje <docwhat@gerf.org>
2017-04-10 01:14:44 -0400

    fixed check for existing plugins

--- tag-rbenv/zsh/startup/rbenv.zsh
+++ tag-rbenv/zsh/startup/rbenv.zsh
@@ -1,16 +1,11 @@
 if [ "${ZSH_ENABLERUBY}" != false ]; then
-  containsElement () {
-    local e
-    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
-    return 1
-  }
-
   if (( $+commands[rbenv] )) && [ -d "${RBENV_ROOT}" ]; then
     mkdir -p "${RBENV_ROOT}/cache"
     if ! (( $+functions[rbenv] )); then
       eval "$(rbenv init - --no-rehash)"
     fi
 
+    # TODO make this smarter
     # Only run rbenv rehash if it was run over 5 minutes ago.
     for i in "${RBENV_ROOT}/shims"(/Nmm+5); do
       touch "${RBENV_ROOT}/shims"
