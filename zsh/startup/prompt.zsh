b279d34ba5f453d4e0bc05db9e6f5b1546bd5053
Christian Höltje <docwhat@gerf.org>
2017-05-04 02:49:28 -0400

    zsh: new prompt — powerlevel9k

--- zsh/startup/prompt.zsh
+++ zsh/startup/prompt.zsh
@@ -1,4 +1,6 @@
-if [[ "${TERM}" != dumb && "${EMACS}" != t ]]; then
-   autoload -Uz promptinit && promptinit
-   prompt docwhat
+if [[ "${TERM}" == dumb || "${EMACS}" == t ]]; then
+   # Disable powerlevel9k
+   add-zsh-hook -D precmd powerlevel9k_\*
+   add-zsh-hook -D preexec powerlevel9k_\*
+   PS1='zsh%# '
 fi
