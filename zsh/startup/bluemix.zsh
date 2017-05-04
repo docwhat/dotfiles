99b24231655a811c5dddd1f235de350528e807fa
Christian Höltje <docwhat@gerf.org>
2017-05-03 21:42:03 -0400

    zsh: bluemix completion

---
+++ zsh/startup/bluemix.zsh
@@ -0,0 +1,8 @@
+# The IBM BlueMix CLI
+# https://console.ng.bluemix.net/docs/cli/reference/bluemix_cli/index.html
+
+# This is where HomeBrew installs it on the Mac.
+# I'm not sure if this is the same on Linux.
+if [ -r /usr/local/Bluemix/bx/zsh_autocomplete ]; then
+  source /usr/local/Bluemix/bx/zsh_autocomplete
+fi
