7ec7645addd68acae4fc34edd44c5fd647dda71f
Christian Höltje <docwhat@gerf.org>
2017-05-06 01:15:11 -0400

    tmux: take advantage of 2.4 features

---
+++ tmux/2.4.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+### Keys
+
+# My favorite prefix.
+tmux set-option -g prefix '`'
+
+# Use vi keys
+tmux set-window-option -g mode-keys vi
+
+tmux bind-key -T copy-mode-vi 'v'   send -X begin-selection
+tmux bind-key -T copy-mode-vi 'V'   send -X select-line
+tmux bind-key -T copy-mode-vi 'C-v' send -X rectangle-toggle
+
+# Screen kill window.
+# unbind k
+tmux bind-key k confirm-before kill-window
+
+# Screen style last window
+# unbind space
+tmux bind-key space last-window
+
+tmux bind-key S command-prompt -p "fssh: " "new-window 'fssh %1'"
+tmux bind-key c new-window -c "#{?pane_current_path,#{pane_current_path},$HOME}"
+
+## Terminal
+tmux set-option -g default-terminal tmux-256color
+tmux set-option -ga terminal-overrides ",xterm-256color:Tc"
+
+## Titles
+tmux set-window -g automatic-rename on
+tmux set-window -g allow-rename off
+
+## Enable mouse support (works in iTerm)
+tmux set-option -g mouse on
+
+# EOF
---
+++ tmux/config.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+tmux_version="$(tmux -V | cut -d ' ' -f 2)"
+tmux_is_at_least() {
+  if [[ $tmux_version == "$1" ]]
+  then
+    return 0
+  fi
+
+  local IFS=.
+  local i tver=($tmux_version) wver=($1)
+
+  # fill empty fields in tver with zeros
+  for ((i=${#tver[@]}; i<${#wver[@]}; i++)); do
+    tver[i]=0
+  done
+
+  # fill empty fields in wver with zeros
+  for ((i=${#wver[@]}; i<${#tver[@]}; i++)); do
+    wver[i]=0
+  done
+
+  for ((i=0; i<${#tver[@]}; i++)); do
+    if ((10#${tver[i]} < 10#${wver[i]})); then
+      return 1
+    fi
+  done
+  return 0
+}
+
+if tmux_is_at_least 2.4; then
+  # shellcheck source=tmux/2.4.sh
+  source './2.4.sh'
+elif tmux_is_at_least 1.9; then
+  # shellcheck source=tmux/1.9.sh
+  source './1.9.sh'
+fi
+
+## Common configurations
+
+# Use SSH-Agent OMZ plugin ###
+tmux set-option -g update-environment "DISPLAY WINDOWID SSH_ASKPASS SSH_AGENT_PID SSH_CONNECTION TERM"
+tmux set-environment -g "SSH_AUTH_SOCK" "$HOME/.ssh/ssh_auth_sock"
+
+# EOF
