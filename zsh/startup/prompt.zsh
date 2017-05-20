5065026d05e39838bdbb69542d3aaa6f5cb142ec
Christian Höltje <docwhat@gerf.org>
2017-05-19 21:13:51 -0400

    zsh: PS2 PS3 and PS4

--- zsh/startup/prompt.zsh
+++ zsh/startup/prompt.zsh
@@ -3,4 +3,53 @@ if [[ "${TERM}" == dumb || "${EMACS}" == t ]]; then
    add-zsh-hook -D precmd powerlevel9k_\*
    add-zsh-hook -D preexec powerlevel9k_\*
    PS1='zsh%# '
+   PS2='%_>'
+   PS3='?# '
+   PS4='+%N:%i> '
+   unset RPROMPT
+   unset RPROMPT2
+else
+   # The secondary prompt, printed when the shell needs more information
+   # to complete a command.  It is expanded in the same way as PS1.
+   # The default is `%_> ', which displays any shell constructs or quotation
+   # marks which are currently being processed.
+   PS2='%s%f%b%k'
+   PS2+='%F{blue}'
+   PS2+='%L:'
+   PS2+='%_'
+   PS2+='%F{green}'
+   PS2+=$'%{\uE0B0%}' # an arrow: 
+   PS2+='%s%f%b%k '
+
+   # Selection prompt used within a select loop.  It is expanded in the same
+   # way as PS1.  The default is `?# '.
+   # http://www.thegeekstuff.com/2008/09/bash-shell-take-control-of-ps1-ps2-ps3-ps4-and-prompt_command/
+   PS3='%s%f%b%k'
+   PS3+='%F{green}'
+   PS3+='select:'
+   PS3+='%s%f%b%k '
+
+   # The execution trace prompt.  Default is `+%N:%i> ', which displays the
+   # name of the current shell structure and the line number within
+   # it. In sh or ksh emulation, the default is `+ '.
+   PS4='%s%f%b%k'
+
+   PS4+=$'%F{blue}'
+   PS4+=$'%{\u03BB%}%L '
+
+   # The name of the script, sourced file, or shell function that zsh is currently executing, whichever was started most recently
+   PS4+='%F{240}'
+   PS4+='%N:%i'
+
+   # The return status of the last command executed just before the prompt
+   PS4+='%(?.. %F{red}%?)'
+
+   # The status of the parser, i.e. the shell constructs (like `if' and `for') that have been started on the command line.
+   PS4+=' %1(_.%F{yellow}%-1_ .)'
+
+   # The end of the prompt
+   # PS4+='%F{blue}'
+   # PS4+=$'%{\u03BB%}'
+   PS4+='%s%f%b%k' # reset all colors
+   PS4+=$'\t'
 fi
