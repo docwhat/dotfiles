6832eb2bb4b824b9e5a3720c808b04898dc6bc73
Christian Höltje <docwhat@gerf.org>
2017-07-26 21:57:55 -0400

    zsh: added some key bindings for vi

--- zsh/startup/edit-mode.zsh
+++ zsh/startup/edit-mode.zsh
@@ -5,6 +5,8 @@
 # function zle-isearch-exit { zle -M "" }
 # zle -N zle-isearch-exit
 
+### Emacs mode
+
 bindkey '^P'  up-history
 bindkey '^N'  down-history
 bindkey '^W'  backward-kill-word
@@ -15,5 +17,14 @@ bindkey "^S" history-incremental-pattern-search-forward
 
 bindkey -M isearch "^R" history-incremental-pattern-search-backward
 bindkey -M isearch "^S" history-incremental-pattern-search-forward
+
+### VI Mode
+
+# VI Mode smash escape
+bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M viins 'kj' vi-cmd-mode
+
+# 10ms for key sequences
+KEYTIMEOUT=1
 
 # EOF
