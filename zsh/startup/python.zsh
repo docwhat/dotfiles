73b0d5c2bac2dd8902a54a7d3936203483931c14
Christian Höltje <docwhat@gerf.org>
2018-02-14 23:59:54 -0500

    zsh:pip fixed check for versions

--- zsh/startup/python.zsh
+++ zsh/startup/python.zsh
@@ -1,11 +1,11 @@
 # Turn off globbing for pip, to allow square brackets.
 # Also, pick the most recent one.
-if [ $+commands[pip3] ]; then
+if (( $+commands[pip3] )); then
   alias pip="noglob pip3"
   alias pip3="noglob pip3"
-elif [ $+commands[pip] ]; then
+elif (( $+commands[pip] )); then
   alias pip="noglob pip"
-elif [ $+commands[pip2] ]; then
+elif (( $+commands[pip2] )); then
   alias pip="noglob pip2"
   alias pip2="noglob pip2"
 fi
