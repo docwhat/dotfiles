de84f858a411edf10c5845df5b850e98da857fd1
Christian Höltje <docwhat@gerf.org>
2018-03-20 23:48:27 -0400

    add LESS option

--- zsh/startup/pagers-editors.zsh
+++ zsh/startup/pagers-editors.zsh
@@ -3,9 +3,9 @@
 # Less Options
 export LESS='-asXFMMRqix8'
 if (( $+commands[lesspipe.sh] )); then
-  export LESSOPEN='| /usr/bin/env lesspipe.sh %s 2>&-'
+  export LESSOPEN='| /usr/bin/env lesspipe.sh %s 2>&-' LESS_ADVANCED_PREPROCESSOR=1
 elif (( $+commands[lesspipe] )); then
-  export LESSOPEN='| /usr/bin/env lesspipe %s 2>&-'
+  export LESSOPEN='| /usr/bin/env lesspipe %s 2>&-' LESS_ADVANCED_PREPROCESSOR=1
 fi
 
 # Pager/Editors
