69fc935f1804ecae8d4826ffd00da4294c0a7c82
Christian Höltje <docwhat@gerf.org>
2018-05-10 14:33:35 -0400

    mdl style tweaks

--- mdlstyle.rb
+++ mdlstyle.rb
@@ -5,14 +5,15 @@ all
 
 rule 'MD007', indent: 4
 rule 'MD029', style: 'ordered'
-rule 'MD004', style: :consistent
 rule 'MD035', style: '---'
 
-exclude_rule 'fenced-code-language' # MD040
-exclude_rule 'first-line-h1'        # MD041
-exclude_rule 'header-style'         # MD029
-exclude_rule 'line-length'          # Line length
-exclude_rule 'list-marker-space'    # Spaces after list markers
-exclude_rule 'no-inline-html'       # MD033
+exclude_rule 'first-header-h1'        # MD002
+exclude_rule 'ul-style'               # MD004
+exclude_rule 'line-length'            # MD013
+exclude_rule 'header-style'           # MD029
+exclude_rule 'list-marker-space'      # MD030
+exclude_rule 'no-inline-html'         # MD033
+exclude_rule 'fenced-code-language'   # MD040
+exclude_rule 'first-line-h1'          # MD041
 
 # EOF
