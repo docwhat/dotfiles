078c878c7878c9ab70ae6531dbf7c793727dbb7b
Christian Höltje <docwhat@gerf.org>
2018-06-13 12:25:30 -0400

    set the PS4 even for shell scripts

--- zsh/startup/prompt.zsh
+++ zsh/startup/prompt.zsh
@@ -36,28 +36,4 @@ else
    PS3+='%F{green}'
    PS3+='select:'
    PS3+='%s%f%b%k '
-
-   # The execution trace prompt.  Default is `+%N:%i> ', which displays the
-   # name of the current shell structure and the line number within
-   # it. In sh or ksh emulation, the default is `+ '.
-   PS4='%s%f%b%k'
-
-   PS4+=$'%F{blue}'
-   PS4+=$'%{\u03BB%}%L '
-
-   # The name of the script, sourced file, or shell function that zsh is currently executing, whichever was started most recently
-   PS4+='%F{240}'
-   PS4+='%N:%i'
-
-   # The return status of the last command executed just before the prompt
-   PS4+='%(?.. %F{red}%?)'
-
-   # The status of the parser, i.e. the shell constructs (like `if' and `for') that have been started on the command line.
-   PS4+=' %1(_.%F{yellow}%-1_ .)'
-
-   # The end of the prompt
-   # PS4+='%F{blue}'
-   # PS4+=$'%{\u03BB%}'
-   PS4+='%s%f%b%k' # reset all colors
-   PS4+=$'\t'
 fi
