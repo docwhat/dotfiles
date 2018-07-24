053af4e44ac2b124d8bdb15c4530f590d8df90b5
Christian Höltje <docwhat@gerf.org>
2018-07-24 17:10:17 -0400

    misc. test files

---
+++ test-files/test.groovy
@@ -0,0 +1,10 @@
+String someFunction(String name, Number count) {
+    println "${name} says '${count}'"
+}
+
+someFunction('Fred', 31415926)
+
+def dictionary = [
+    fred: true,
+    gracie: false
+]
