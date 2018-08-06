053972482f8a6326f6c2494311365e10e5196717
Christian Höltje <docwhat@gerf.org>
2018-08-06 10:04:03 -0400

    hammerspoon: trying it out for capslock->control-escape

---
+++ tag-macos/hammerspoon/init.lua
@@ -0,0 +1,5 @@
+-- https://www.hammerspoon.org/
+-- Getting Started Guide: https://www.hammerspoon.org/go/
+
+-- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+hs.loadSpoon('ControlEscape'):start()
