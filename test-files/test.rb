eb02b4d4e0015ccb809dbdedf13d448a968b0145
Christian Höltje <docwhat@gerf.org>
2018-08-21 11:35:16 -0400

    test: made rb file more interesting

--- test-files/test.rb
+++ test-files/test.rb
@@ -1,15 +1,18 @@
-{{{}}}
-((()))
-[[[]]]
-[[[[]]]]
+SOME_CONSTANT = 'This is a string from the edge of the world.'.freeze
 
-def sample_function(a, b)
-    ((()))
-    [[[]]]
+def sample_function(aaa, bbb)
+  new_a = "Some string: #{aaa}"
+  new_b = "Another string #{bbb}"
+
+  unused_variable = 123
+
+  puts "#{new_a} #{new_b}"
 end
 
+##
+# This is a Class
 class SampleClass
-    def sample_method(a, b)
-        [[[]]]
-    end
+  def sample_method(a, b)
+    puts "#{a} #{b}"
+  end
 end
