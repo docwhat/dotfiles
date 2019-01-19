5d47142c181d049cf30c2006b8b361940a4f7bc9
Christian Höltje <docwhat@gerf.org>
2019-01-18 19:25:47 -0500

    rg: always use pager

--- zsh/startup/ack.zsh
+++ zsh/startup/ack.zsh
@@ -4,6 +4,12 @@
 # switched search tools I didn't
 # rename the file.
 
+if (( $+commands[rg] )); then
+  function rg() {
+    =rg --pretty "$@" | less -RFX
+  }
+fi
+
 if (( $+commands[ag] )); then
   alias  ag="ag --pager 'less -RS' --smart-case -C3 --color-line-number '1;34' --color-match '1;04;31' --color-path '1;32'"
 fi
