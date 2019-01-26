0a942d30912032a3c2452a5deee23aef76383d75
Christian Höltje <docwhat@gerf.org>
2019-01-26 00:25:02 -0500

    fzf tweak settings

--- zsh/startup/fzf.zsh
+++ zsh/startup/fzf.zsh
@@ -1,31 +1,12 @@
-
 if [ -d ~/.fzf ]; then
-  export FZF_DEFAULT_OPTS='
-  --color fg:#e8e8d3,bg:-1,hl:#ff8800,fg+:-1,bg+:#404040,hl+:#ffff00
-  --color info:#ddaaff,prompt:#88aadd,spinner:#88aa55,pointer:#dd5555,marker:#ffaa55
-  '
-
-  export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --inline-info --bind=ctrl-k:kill-line"
-
-  export FZF_CTRL_R_OPTS="--with-nth=2.. --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview' --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' --header 'Press CTRL-Y to copy command into clipboard'"
-
-  # Enable FZF via TMUX pane
-  if [ -n "$TMUX" ]; then
-    export FZF_TMUX=1
-  else
-    export FZF_CTRL_R_OPTS="${FZF_CTRL_R_OPTS} --border"
-  fi
-
   source ~/.fzf/shell/completion.zsh
   source ~/.fzf/shell/key-bindings.zsh
-
-  export FZF_COMPLETION_TRIGGER=''
-  bindkey '^T' fzf-completion
-  bindkey '^I' $fzf_default_completion
+  # bindkey '^T' fzf-completion
+  # bindkey '^I' $fzf_default_completion
 
   #   - CTRL-O to open with `open` command,
   #   - CTRL-E or Enter key to open with the $EDITOR
-  function fe {
+  function fe() {
     local out file key
     local ed="${EDITOR:-vim}"
     IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e --header "Press CTRL-O to use open, Enter or CTRL-E to use ${ed}"))
@@ -41,5 +22,21 @@ if [ -d ~/.fzf ]; then
     fi
   }
 
+  if (( $+commands[fd] )); then
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    function _fzf_compgen_path() {
+      fd --hidden --follow --exclude ".git" . "$1"
+    }
+
+    # Use fd to generate the list for directory completion
+    function _fzf_compgen_dir() {
+      fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+  fi
+
   manpath=( "${manpath[@]}" ~/.fzf/man )
 fi
+# vim: set ft=zsh :
--- zsh/startup/fzf.zshenv
+++ zsh/startup/fzf.zshenv
@@ -1,5 +1,36 @@
-
 # Add FZF to path
 if [ -d ~/.fzf ]; then
   path=("${path[@]}" ~/.fzf/bin)
+
+  export FZF_COMPLETION_TRIGGER='**'
+  declare -a fzf_default_opts
+
+  fzf_default_opts=(
+    '--color=fg:-1,bg:-1,hl:#ff8800,fg+:-1,bg+:#404040,hl+:#ffff00'
+    '--color=info:#ddaaff,prompt:#88aadd,spinner:#88aa55,pointer:#dd5555,marker:#ffaa55'
+
+    '--inline-info'
+    '--bind=ctrl-k:kill-line'
+  )
+
+  fzf_ctrl_r_opts=(
+  '--with-nth=2..'
+  '--preview=echo {}'
+  '--preview-window=down:3:hidden:wrap'
+  '--bind=?:toggle-preview'
+  '--bind=ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  '--header=Press CTRL-Y to copy command into clipboard'
+  )
+
+  # Enable FZF via TMUX pane
+  if [ -n "$TMUX" ]; then
+    export FZF_TMUX=1
+  else
+    fzf_ctrl_r_opts+=( '--border' )
+  fi
+
+  export FZF_CTRL_R_OPTS="${(@q-)fzf_ctrl_r_opts[@]}"
+  export FZF_DEFAULT_OPTS="${(@q-)fzf_default_opts[@]}"
+  unset fzf_default_opts fzf_ctrl_r_opts
 fi
+# vim: set ft=zsh :
