dcf4c61a14a1042599534a3010e2bed49eca4401
Christian Höltje <docwhat@gerf.org>
2019-07-03 16:24:01 -0400

    zsh: new highlighting

--- zsh/startup/zsh.zsh
+++ zsh/startup/zsh.zsh
@@ -36,6 +36,20 @@ setopt dvorak
 unsetopt correctall
 setopt correct
 
+if is-at-least 5.1; then
+  set zle_bracketed_paste  # Explicitly restore this zsh default
+  autoload -Uz bracketed-paste-magic
+  zle -N bracketed-paste bracketed-paste-magic
+fi
+
+if is-at-least 4.3; then
+  typeset -ga zle_highlight
+  zle_highlight=(
+    isearch:underline
+    paste:bg=34,fg=0
+  )
+fi
+
 alias copy-last-command='fc -l -n -1 -1 | tr -d "\n" | pbcopy'
 if (( $+commands[resize] )); then
   alias resize='eval "$(\resize)"'
