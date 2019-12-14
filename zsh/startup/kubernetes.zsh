5f57c7e560a9888ffc5101dc0904c9256e98570c
Christian Holtje <docwhat@gerf.org>
2019-12-14 00:25:50 -0500

    zsh: k as alias for kubectl

--- zsh/startup/kubernetes.zsh
+++ zsh/startup/kubernetes.zsh
@@ -5,9 +5,9 @@ autoload -U is-at-least
 if is-at-least 5.2; then
   # Get kubectl completion
   if (( $+commands[kubectl] )); then
-    {
-      source <(kubectl completion zsh)
-    } &!
+    source <(kubectl completion zsh)
+    alias k=kubectl
+    complete -F __start_kubectl k
   fi
 
   if (( $+commands[tkn] )); then
