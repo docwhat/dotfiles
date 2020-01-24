5887c5af84b6c5e20dcf7c1a9d4e7cc1ef7a6812
Christian Höltje <docwhat@gerf.org>
2020-01-24 14:31:58 -0500

    test(js): add more lint

--- test-files/example.js
+++ test-files/example.js
@@ -9,10 +9,26 @@ const letters = ["a", "b", "c", "d", "e"]
 console.log(letters.join("-"))
 
 const newObject = {
-    a: 'a.m.', b: 'p.m.',
-    c: 'daylight saving time'
-};
+  a: "a.m.",
+  b: "p.m.",
+  c: "daylight saving time"
+}
+
+var a = 10
+var b = 024
 
 if ((a = b)) {
   console.log("bad assignment in conditional")
 }
+
+function say(a, b) {
+  print(a + " " + b)
+}
+
+say("hello", "world", "!") // Noncompliant; last argument is not used
+
+var merged = arr.reduce(function(a, b) {
+  a.concat(b)
+})
+
+// EOF
