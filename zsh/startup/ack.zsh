ad22bd64faca14701f24b94634b2e741644b0c12
Christian Höltje <docwhat@gerf.org>
2020-10-02 15:25:07 -0400

    rg does not need to force the pager on

--- zsh/startup/ack.zsh
+++ zsh/startup/ack.zsh
@@ -4,16 +4,6 @@
 # switched search tools I didn't
 # rename the file.
 
-if (( $+commands[rg] )); then
-  function rg() {
-    if [ -t 1 ]; then
-      =rg --pretty "$@" | less -RFX
-    else
-      =rg "$@"
-    fi
-  }
-fi
-
 if (( $+commands[ag] )); then
   alias  ag="ag --pager 'less -RS' --smart-case -C3 --color-line-number '1;34' --color-match '1;04;31' --color-path '1;32'"
 fi
