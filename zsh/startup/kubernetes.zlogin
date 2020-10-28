767c0ae1438ecfec1a3dd2589c443e2ac2030ad6
Christian Höltje <docwhat@gerf.org>
2020-10-28 13:08:50 -0400

    zsh(kubernetes): moved completion generation to zlogin

---
+++ zsh/startup/kubernetes.zlogin
@@ -0,0 +1,31 @@
+#!zsh
+
+autoload -U is-at-least
+
+if is-at-least 5.2; then
+  # Get kubectl completion
+  if (( $+commands[kubectl] )) && \
+    [[ -n ~/.zsh/functions/_kubectl(#qN.mh+24) ]]; then
+      {
+      env KUBECONFIG= \
+        kubectl completion zsh \
+        > ~/.zsh/functions/_kubectl
+      } &!
+  fi
+
+  if (( $+commands[tkn] )) && \
+    [[ -n ~/.zsh/functions/_tkn(#qN.mh+24) ]]; then
+      {
+        tkn completion zsh \
+        > ~/.zsh/functions/_tkn
+      } &!
+  fi
+
+  if (( $+commands[helm] )) && \
+    [[ -n ~/.zsh/functions/_helm(#qN.mh+24) ]]; then
+      {
+        helm completion zsh \
+        > ~/.zsh/functions/_helm
+      } &!
+  fi
+fi
--- zsh/startup/kubernetes.zsh
+++ zsh/startup/kubernetes.zsh
@@ -1,7 +1,5 @@
 #!zsh
 
-autoload -U is-at-least
-
 # Keep the configs secure.
 if ! [[ -d ~/.kube/config.d ]]; then
   mkdir -m 0700 -p ~/.kube/config.d
@@ -9,32 +7,4 @@ fi
 
 if [[ -f ~/.kube/config ]]; then
   chmod 0600 ~/.kube/config
-fi
-
-if is-at-least 5.2; then
-  # Get kubectl completion
-  if (( $+commands[kubectl] )) && \
-    [[ -n ~/.zsh/functions/_kubectl(#qN.mh+24) ]]; then
-      {
-      env KUBECONFIG= \
-        kubectl completion zsh \
-        > ~/.zsh/functions/_kubectl
-      } &!
-  fi
-
-  if (( $+commands[tkn] )) && \
-    [[ -n ~/.zsh/functions/_tkn(#qN.mh+24) ]]; then
-      {
-        tkn completion zsh \
-        > ~/.zsh/functions/_tkn
-      } &!
-  fi
-
-  if (( $+commands[helm] )) && \
-    [[ -n ~/.zsh/functions/_helm(#qN.mh+24) ]]; then
-      {
-        helm completion zsh \
-        > ~/.zsh/functions/_helm
-      } &!
-  fi
 fi
