2699c0a2cbe07ebefda8ba9b0810d5ac49f250a9
Christian Höltje <docwhat@gerf.org>
2020-11-24 12:16:33 -0500

    zsh: setup starship completions

--- tag-prompt-starship/zsh/startup/zz-starship.zsh
+++ tag-prompt-starship/zsh/startup/zz-starship.zsh
@@ -2,5 +2,6 @@ zinit ice from"gh-r" as"program"
 zinit light starship/starship
 
 if (( $+commands[starship] )); then
+  starship completions zsh > ~/.zsh/functions/_starship &!
   eval "$(starship init zsh)"
 fi
