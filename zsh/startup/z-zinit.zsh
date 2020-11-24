8960cf74eaa70e99466e3d9f0aafaab4d3f2ea90
Christian Höltje <docwhat@gerf.org>
2020-11-24 15:32:15 -0500

    zsh: lots more zinit goodies

--- zsh/startup/z-zinit.zsh
+++ zsh/startup/z-zinit.zsh
@@ -13,33 +13,80 @@ function from-where {
 declare -a zinit_plugins=(
   id-as"z.lua"
   has"lua"
-  skywind3000/z.lua
+  @skywind3000/z.lua
 
   id-as"git-extras"
   has"git"
   src"etc/git-extras-completion.zsh"
   make"PREFIX=$ZPFX"
-  tj/git-extras
+  @tj/git-extras
   # TODO: is it a bug that 'zi clist' does not show completions?
 
   id-as"256color"
-  chrissicool/zsh-256color
+  @chrissicool/zsh-256color
 
   id-as"completion-generator"
-  RobSis/zsh-completion-generator
+  @RobSis/zsh-completion-generator
 
   id-as"fast-syntax-highlighting"
   atinit"ZINIT[COMPINIT_OPTS]='-i -d "${ZSH_COMPDUMP}"'; zicompinit; zicdreplay"
-  zdharma/fast-syntax-highlighting
+  @zdharma/fast-syntax-highlighting
+
+	id-as"zsh-autopair"
+  @hlissner/zsh-autopair
+
+  id-as"alias-tips"
+  pick"alias-tips.plugin.zsh"
+  atload'export ZSH_PLUGINS_ALIAS_TIPS_REVEAL=1'
+  @djui/alias-tips
+
+	id-as"fzf-tab"
+	has"fzf"
+	atload"zstyle ':fzf-tab:complete:cd:*' fzf-preview --height=100% 'lsd -1 --color=always $realpath'"
+  @Aloxaf/fzf-tab
+
+	id-as"bat"
+	as"program"
+	from"gh-r"
+	mv"bat-* -> bat"
+	atclone'mv bat/bat.1 "$ZPFX/share/man/man1/" && mv bat/autocomplete/bat.zsh bat/autocomplete/_bat'
+	atpull'%atclone'
+	pick"bat"
+	atload'alias less="bat"'
+  @sharkdp/bat
+
+  id-as"fd"
+  as"program"
+  from"gh-r"
+  mv"fd-* -> fd"
+	atclone'mv fd/fd.1 "$ZPFX/share/man/man1/"'
+	atpull'%atclone'
+  atinit'source fd/autocomplete/fd.bash'
+  pick"fd"
+  @sharkdp/fd
+
+  id-as"bat-extras"
+  as"program"
+  atclone'./build.sh --prefix "$ZPFX" --install && mv man/*.1 "$ZPFX/share/man/man1"'
+	atpull'%atclone'
+  @eth-p/bat-extras
+
+	id-as"lsd"
+	as"program"
+	from"gh-r"
+	mv"lsd* -> lsd"
+	pick"lsd/lsd"
+	atload'alias ls="lsd"'
+  @Peltoche/lsd
 
   id-as"autosuggestions"
   atload"_zsh_autosuggest_start"
-  zsh-users/zsh-autosuggestions
+  @zsh-users/zsh-autosuggestions
 
   id-as"completions"
   atpull'zinit creinstall -q .'
   blockf
-  zsh-users/zsh-completions
+  @zsh-users/zsh-completions
 )
 
 # macOS items
