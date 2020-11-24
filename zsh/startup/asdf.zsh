7a569e65e9a87fa463e5dce9772ce3bce90e423b
Christian Höltje <docwhat@gerf.org>
2020-11-24 15:27:26 -0500

    zsh: remove old asdf setup code

--- zsh/startup/asdf.zsh
+++
@@ -1,7 +0,0 @@
-if (( $+commands[asdf] )) && [[ -d "$ASDF_DIR" ]]; then
-  alias aoeu=asdf
-  source "${ASDF_DIR}/asdf.sh"
-  source "${ASDF_DIR}/etc/bash_completion.d/asdf.bash"
-fi
-
-# vim: set ft=zsh :
--- zsh/startup/asdf.zshenv
+++
@@ -1,7 +0,0 @@
-if [[ "${OSTYPE}" == darwin*  ]] && [[ -d /usr/local/opt/asdf ]]; then
-  export ASDF_DIR=/usr/local/opt/asdf
-elif [[ -d ~/.asdf ]]; then
-  ASDF_DIR=~/.asdf
-fi
-
-# vim: set ft=zsh :
