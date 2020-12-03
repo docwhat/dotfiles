40fea57c196492adae0b490e777915e804c9e2b2
Christian Holtje <docwhat@gerf.org>
2020-12-02 21:34:30 -0500

    zsh: cleanup starship zinit

--- tag-prompt-starship/zsh/startup/zz-starship.zsh
+++ tag-prompt-starship/zsh/startup/zz-starship.zsh
@@ -1,7 +1,13 @@
-zinit ice id-as"starship" from"gh-r" as"program"
+zinit ice \
+  --id-as=starship \
+  --from=gh-r \
+  --as=program \
+  --atclone='starship completions zsh > ~/.zsh/functions/_starship' \
+  --atpull='%atclone' \
+  --atload='!eval $(starship init zsh)'
+
 zinit light starship/starship
 
-if (( $+commands[starship] )); then
-  starship completions zsh > ~/.zsh/functions/_starship &!
+if (( ${+commands[starship]} )); then
   eval "$(starship init zsh)"
 fi
