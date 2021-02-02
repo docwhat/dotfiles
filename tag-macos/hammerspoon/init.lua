353f88d8dabd99722f80049d642150ba67208298
Christian Höltje <docwhat@gerf.org>
2021-02-02 18:15:37 -0500

    lua: setup formatting in vscode

--- tag-macos/hammerspoon/init.lua
+++ tag-macos/hammerspoon/init.lua
@@ -3,9 +3,7 @@
 --    https://www.hammerspoon.org/go/
 -- Learn X in Y minutes; where X = Lua:
 --    https://learnxinyminutes.com/docs/lua/
-
 -- luacheck: globals hs spoon
-
 -- Make the console dark
 hs.console.darkMode(true)
 if hs.console.darkMode() then
@@ -16,56 +14,36 @@ if hs.console.darkMode() then
 end
 
 -- luacheck: globals hyper shift_hyper Install
-hyper = {"cmd", "alt", "ctrl"}
-shift_hyper = {"cmd", "alt", "ctrl", "shift"}
+local hyper = {"cmd", "alt", "ctrl"}
+local shift_hyper = {"cmd", "alt", "ctrl", "shift"}
 Install = hs.loadSpoon("SpoonInstall")
 -- Install.use_syncinstall = true
 
 spoon.SpoonInstall.repos.zzspoons = {
   url = "https://github.com/zzamboni/zzSpoons",
-  desc = "zzamboni's spoon repository"
+  desc = "zzamboni's spoon repository",
 }
 
 -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 -- I need to figure out how to load this via SpoonInstall.
-hs.loadSpoon("ControlEscape"):start()
+Install:andUse("ControlEscape", {start = true})
 
-Install:andUse(
-  "Hammer",
-  {
-    repo = "zzspoons",
-    -- config = {auto_reload_config = false},
-    hotkeys = {
-      config_reload = {hyper, "r"},
-      toggle_console = {hyper, "y"}
-    },
-    start = true
-  }
-)
+Install:andUse("Hammer", {
+  repo = "zzspoons",
+  -- config = {auto_reload_config = false},
+  hotkeys = {config_reload = {hyper, "r"}, toggle_console = {hyper, "y"}},
+  start = true,
+})
 
-Install:andUse(
-  "SleepCorners",
-  {
-    config = {
-      neverSleepCorner = "LR",
-      sleepNowCorner = "LL"
-    },
-   start = true
-  }
-)
+Install:andUse("SleepCorners", {
+  config = {neverSleepCorner = "LR", sleepNowCorner = "LL"},
+  start = true,
+})
 -- -- Handy for debugging. From:
 -- --    https://github.com/kikito/inspect.lua/blob/master/inspect.lua
 -- -- luacheck: globals inspect
 -- inspect = require("inspect")
 
-Install:andUse(
-  "FadeLogo",
-  {
-    config = {
-      default_run = 1.0
-    },
-    start = true
-  }
-)
+Install:andUse("FadeLogo", {config = {default_run = 1.0}, start = true})
 
 -- EOF
