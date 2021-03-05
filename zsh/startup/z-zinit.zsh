203c1617d6b6ce057cde85582493927dad6b20b3
Christian Höltje <docwhat@gerf.org>
2021-03-05 10:21:22 -0500

    zsh(zi): more exa goodness

--- zsh/startup/z-zinit.zsh
+++ zsh/startup/z-zinit.zsh
@@ -139,7 +139,9 @@ declare -a zinit_programs=(
   from"gh-r"
   mv"exa* -> exa"
   pick"exa/exa"
-  atload'alias ls="exa --icons --time-style=iso --git"'
+  atload'alias ls="exa --icons --time-style=iso --git --classify --color-scale --color=auto"'
+  atload'alias ll="exa --icons --time-style=iso --git --classify --color-scale --color=auto --long -i --extended"'
+  atload'alias tree="exa --icons --time-style=iso --git --classify --color-scale --color=auto --tree"'
   @ogham/exa
 
 
