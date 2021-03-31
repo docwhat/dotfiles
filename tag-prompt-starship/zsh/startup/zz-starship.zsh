32e7c3951cdeb1ba35d1ce153b03e03e96e77c1b
Christian Holtje <docwhat@gerf.org>
2021-03-31 16:23:01 -0400

    zsh: starship sets kitty tab titles

--- tag-prompt-starship/zsh/startup/zz-starship.zsh
+++ tag-prompt-starship/zsh/startup/zz-starship.zsh
@@ -10,4 +10,19 @@ zinit light starship/starship
 
 if (( ${+commands[starship]} )); then
   eval "$(starship init zsh)"
+
+  if (( ${+commands[kitty]} )); then
+    function docwhat-starship-precmd() {
+      # strip color and trailing space
+      kitty @ set-tab-title "$(starship module directory | sed $'s/\e\[[0-9;]*m//g; s/ $//g')"
+    }
+    precmd_functions+=(docwhat-starship-precmd)
+
+    function docwhat-starship-preexec() {
+      kitty @ set-tab-title "$1"
+    }
+    preexec_functions+=(docwhat-starship-preexec)
+  fi
+
+  declare -Ua preexec_functions precmd_functions
 fi
