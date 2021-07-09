7a07cf21abffc9e7192488df16cb2e3662de4e94
Christian Höltje <docwhat@gerf.org>
2021-07-09 11:02:08 -0400

    remove uneeded workaround

--- zsh/startup/docker.zsh
+++ zsh/startup/docker.zsh
@@ -1,10 +1,5 @@
 # vim: ft=zsh :
 
-## Ensure docker-compose doesn't get messed up by certificate settings.
-if (( ${+commands[docker-compose]} )); then
-  alias docker-compose="env -u REQUESTS_CA_BUNDLE docker-compose"
-fi
-
 # Set up default docker-machine, unless Docker.app is available.
 if [ ! -d /Applications/Docker.app ] && (( ${+commands[docker-machine]} )); then
   if [ -n "$(docker-machine ls --filter='state=running' --filter='name=default' --format='ok' 2>/dev/null)" ]; then
