ed15c792a1ed58105601c99d26c9db0ec5ef5614
Christian Höltje <docwhat@gerf.org>
2021-11-17 22:59:18 -0500

    fix(brew): homebrew has moved by default to /opt

--- zsh/startup/homebrew-env.zsh
+++ zsh/startup/homebrew-env.zsh
@@ -1,2 +1,15 @@
-
 # vim: set ft=zsh :
+
+for __brewbin in /opt/homebrew/bin/brew /usr/local/bin/brew; do
+  if [[ -x "$__brewbin" ]]; then
+    eval "$("$__brewbin" shellenv zsh)"
+
+    # git's contrib completion for zsh is ... sub-optimal.
+    # Homebrew used to allow ditching it, but no more.
+    # https://github.com/Homebrew/homebrew-core/issues/33275
+    # This ensures homebrew goes early in the fpath.
+    fpath=("$HOMEBREW_PREFIX/share/zsh/functions" "${fpath[@]}")
+    break
+  fi
+done
+unset __brewbin
--- zsh/startup/homebrew-rc.zsh
+++ zsh/startup/homebrew-rc.zsh
@@ -2,19 +2,12 @@
 if (( ${+commands[brew]} )); then
 
   # Autojump brew
-  if [[ -f "$(brew --prefix)/etc/autojump" ]]; then
-    source "$(brew --prefix)/etc/autojump"
+  if [[ -f "$HOMEBREW_PREFIX/etc/autojump" ]]; then
+    source "$HOMEBREW_PREFIX/etc/autojump"
   fi
 
-  if [ -x /usr/local/opt/openssl/bin/openssl ]; then
-    alias openssl=/usr/local/opt/openssl/bin/openssl
-  fi
-
-  # git's contrib completion for zsh is ... sub-optimal.
-  # Homebrew used to allow ditching it, but no more.
-  # https://github.com/Homebrew/homebrew-core/issues/33275
-  if ! [[ ~/.zsh/functions/_git -ef /usr/local/opt/zsh/share/zsh/functions/_git ]]; then
-    ln -nfs /usr/local/opt/zsh/share/zsh/functions/_git ~/.zsh/functions/_git
+  if [ -x "$HOMEBREW_PREFIX/opt/openssl/bin/openssl" ]; then
+    alias openssl="$HOMEBREW_PREFIX/opt/openssl/bin/openssl"
   fi
 fi
 
