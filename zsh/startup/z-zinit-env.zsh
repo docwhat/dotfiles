0a9117315d9d8d8802ea344eea67a72fae226e89
Christian Höltje <docwhat@gerf.org>
2022-03-15 12:25:04 -0400

    fix: workaround for gencompl on macOS 12.3
    completion-generator doesn't detect python on systems with only python3
    binaries.
    
    macOS 12.3 now no longer has a python executable; python 2.x has been
    completely removed.
    
    See: RobSis/zsh-completion-generator#19

--- zsh/startup/z-zinit-env.zsh
+++ zsh/startup/z-zinit-env.zsh
@@ -14,6 +14,10 @@ export _ZL_MATCH_MODE=1
 export _ZL_ROOT_MARKERS=".git,.svn,.hg,.root,package.json"
 
 # RobSis/zsh-completion-generator
+if [[ -z $GENCOMPL_PY ]] && [[ -z "${commandds[python]}" ]] && [[ -n "${commands[python3]}" ]]; then
+  # It doesn't auto-detect 'python3' when 'python' is missing.
+  GENCOMPL_PY=python3
+fi
 GENCOMPL_FPATH="${ZSH_CACHE_DIR}/completion-generator"
 
 # EOF
