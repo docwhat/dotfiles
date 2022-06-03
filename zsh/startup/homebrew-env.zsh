c029638969c248b15e4484e315b653f7fb43eee0
Christian Höltje <docwhat@gerf.org>
2022-06-03 15:49:03 -0400

    zsh: fix blank entries in INFOPATH and MANPATH

--- zsh/startup/homebrew-env.zsh
+++ zsh/startup/homebrew-env.zsh
@@ -1,15 +1,23 @@
 # vim: set ft=zsh :
 
-for __brewbin in /opt/homebrew/bin/brew /usr/local/bin/brew; do
-  if [[ -x "$__brewbin" ]]; then
-    eval "$("$__brewbin" shellenv zsh)"
+function {
+  local brewbin
+  for brewbin in /opt/homebrew/bin/brew /usr/local/bin/brew; do
+    if [[ -x "$brewbin" ]]; then
+      eval "$("$brewbin" shellenv zsh)"
+      INFOPATH=${INFOPATH%:}
+      MANPATH=${MANPATH%:}
 
-    # git's contrib completion for zsh is ... sub-optimal.
-    # Homebrew used to allow ditching it, but no more.
-    # https://github.com/Homebrew/homebrew-core/issues/33275
-    # This ensures homebrew goes early in the fpath.
-    fpath=("$HOMEBREW_PREFIX/share/zsh/functions" "${fpath[@]}")
-    break
-  fi
-done
-unset __brewbin
+      local brewfpath
+      brewfpath=${${:-"${HOMEBREW_PREFIX}/share/zsh/functions/_git"}:A:h}
+      if [[ -d "$brewfpath" ]] && (( ${fpath[(i)$brewfpath]} <= ${#fpath} )) ; then
+        # git's contrib completion for zsh is ... sub-optimal.
+        # Homebrew used to allow ditching it, but no more.
+        # https://github.com/Homebrew/homebrew-core/issues/33275
+        # This ensures homebrew goes early in the fpath.
+        fpath=($brewfpath "${fpath[@]}")
+      fi
+      break
+    fi
+  done
+}
