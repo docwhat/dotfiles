48e0b797e0f42315c6fef12d21f8ebeba2de5d53
Christian Höltje <christian.holtje@boxcast.com>
2022-10-12 15:14:47 -0400

    add rancher desktop to PATH

---
+++ bash/startup/docker-rc.bash
@@ -0,0 +1,10 @@
+# vim: ft=bash
+
+# Rancher Desktop
+if [[ -d "$HOME/.rd/bin" ]]; then
+  PATH="$PATH:$HOME/.rd/bin"
+fi
+
+export PATH
+
+# BASH
--- zsh/startup/docker-env.zsh
+++ zsh/startup/docker-env.zsh
@@ -3,3 +3,8 @@
 if (( ${+commands[docker]} )); then
   export DOCKER_BUILDKIT=1
 fi
+
+# Rancher Desktop
+if [[ -d "$HOME/.rd/bin" ]]; then
+  path+=("$HOME/.rd/bin")
+fi
