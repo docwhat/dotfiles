251349f566e2b9e0c9343d49051634cf1c3607bc
Christian Höltje <christian.holtje@boxcast.com>
2022-11-28 16:33:33 -0500

    gcloud: move completion into rc file

--- zsh/startup/gcloud-env.zsh
+++ zsh/startup/gcloud-env.zsh
@@ -1,7 +1,3 @@
-if [[ -f "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
-  source "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-fi
-
 if [[ -f "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]]; then
   source "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 fi
---
+++ zsh/startup/gcloud-rc.zsh
@@ -0,0 +1,4 @@
+if [[ -f "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
+  source "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
+
