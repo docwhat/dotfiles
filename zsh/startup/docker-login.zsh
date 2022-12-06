3ebca2378b9a2c6288871df297fe29c5bd947a50
Christian Höltje <christian.holtje@boxcast.com>
2022-12-06 15:35:45 -0500

    remove my custom kubectl wrapper in favor of kuberlr

---
+++ zsh/startup/docker-login.zsh
@@ -0,0 +1,21 @@
+#!zsh
+
+autoload -U is-at-least
+
+if is-at-least 5.2; then
+  if (( ${+commands[nerdctl]} )) && \
+    [[ -n ~/.zsh/functions/_nerdctl(#qN.mh+24) ]]; then
+      {
+        nerdctl completion zsh \
+        > ~/.zsh/functions/_nerdctl
+      } &!
+  fi
+
+  if (( ${+commands[rdctl]} )) && \
+    [[ -n ~/.zsh/functions/_rdctl(#qN.mh+24) ]]; then
+      {
+        rdctl completion zsh \
+        > ~/.zsh/functions/_rdctl
+      } &!
+  fi
+fi
