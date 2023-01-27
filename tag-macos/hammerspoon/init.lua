3f10c23561b42b07fa74ce9e477f6954f346ec52
Christian Höltje <docwhat@gerf.org>
2023-01-27 10:42:37 -0500

    macos: improve quake term like feel of kitty

--- tag-macos/hammerspoon/init.lua
+++ tag-macos/hammerspoon/init.lua
@@ -7,14 +7,14 @@
 -- Make the console dark
 hs.console.darkMode(true)
 if hs.console.darkMode() then
-  hs.console.outputBackgroundColor {white = 0}
-  hs.console.consoleCommandColor {white = 1}
-  hs.console.consolePrintColor {white = 1}
+  hs.console.outputBackgroundColor { white = 0 }
+  hs.console.consoleCommandColor { white = 1 }
+  hs.console.consolePrintColor { white = 1 }
   hs.console.alpha(1)
 end
 
 -- luacheck: globals hyper shift_hyper Install
-local hyper = {"cmd", "alt", "ctrl"}
+local hyper = { "cmd", "alt", "ctrl" }
 -- local shift_hyper = {"cmd", "alt", "ctrl", "shift"}
 Install = hs.loadSpoon("SpoonInstall")
 -- Install.use_syncinstall = true
@@ -26,41 +26,43 @@ spoon.SpoonInstall.repos.zzspoons = {
 
 -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 -- I need to figure out how to load this via SpoonInstall.
-Install:andUse("ControlEscape", {start = true})
+Install:andUse("ControlEscape", { start = true })
 
 Install:andUse("Hammer", {
-    repo = "zzspoons",
-    -- config = {auto_reload_config = false},
-    hotkeys = {config_reload = {hyper, "r"}, toggle_console = {hyper, "y"}},
-    start = true,
-  })
+  repo = "zzspoons",
+  -- config = {auto_reload_config = false},
+  hotkeys = { config_reload = { hyper, "r" }, toggle_console = { hyper, "y" } },
+  start = true,
+})
 -- -- Handy for debugging. From:
 -- --    https://github.com/kikito/inspect.lua/blob/master/inspect.lua
 -- -- luacheck: globals inspect
 -- inspect = require("inspect")
 
 -- hs.hotkey.bind({"ctrl", "alt"}, "space", function()
-hs.hotkey.bind({"alt"}, "return", function()
+hs.hotkey.bind({ "alt" }, "return", function()
+  local kittyID = "net.kovidgoyal.kitty"
   local app = hs.application.get("kitty")
 
   if app then
     if not app:mainWindow() then
-      app:selectMenuItem({"kitty", "New OS window"})
+      app:selectMenuItem({ "kitty", "New OS window" })
     elseif app:isFrontmost() then
       app:hide()
     else
       app:activate()
     end
   else
-    hs.application.launchOrFocus("kitty")
-    app = hs.application.get("kitty")
+    app = hs.application.open(kittyID, 10, true)
+    if not app then
+      return
+    end
   end
 
-  if app then
-    app:mainWindow():moveToUnit'[99.5,50,0.5,0]'
-  end
+  app:mainWindow():moveToUnit '[99.9,50,0.1,0]'
+  app:mainWindow().setShadows(false)
 end)
 
-Install:andUse("FadeLogo", {config = {default_run = 1.0}, start = true})
+Install:andUse("FadeLogo", { config = { default_run = 1.0 }, start = true })
 
 -- EOF
