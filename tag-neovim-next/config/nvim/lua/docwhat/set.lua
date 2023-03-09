25664f100f8712209ceff1e21e334333de9fbb71
Christian Höltje <docwhat@gerf.org>
2023-03-08 23:49:05 -0500

    nvim: more tweaks from LazyVim

--- tag-neovim-next/config/nvim/lua/docwhat/set.lua
+++ tag-neovim-next/config/nvim/lua/docwhat/set.lua
@@ -1,32 +1,52 @@
-vim.opt.number = true
+local o = vim.opt
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 0
-vim.opt.softtabstop = -1
-vim.opt.expandtab = true
-vim.opt.shiftround = true
+-- Enable auto write
+o.autowrite = true
+
+-- Show the line number
+o.number = true
+
+o.tabstop = 2
+o.shiftwidth = 0
+o.softtabstop = -1
+o.expandtab = true
+o.shiftround = true
 
 -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+o.ignorecase = true
+o.smartcase = true
+
+-- Hilight the line where the cursor The ISC License (ISC)
+o.cursorline = true
+
+o.pumblend = 10 -- Popup blend
+o.pumheight = 10 -- Maximum number of entries in a popup
 
 -- Save undo history
-vim.opt.undofile = true
+o.undofile = true
 
 -- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeout = true
-vim.opt.timeoutlen = 500
+o.updatetime = 250
+o.timeout = true
+o.timeoutlen = 500
 
 -- When scrolling, try not to get closer than this to the top and bottom.
-vim.opt.scrolloff = 6
+o.scrolloff = 6
 
-vim.opt.signcolumn = "yes"
+-- Always show the signcolumn, otherwise it would shift the text each time
+o.signcolumn = "yes"
 
 -- Draw a bar to show where the 80 character column is.
-vim.opt.colorcolumn = "80"
+o.colorcolumn = "80"
+
+-- Command-line completion mode
+o.wildmode = "longest:full,full"
+
+o.shortmess:append({ W = true, I = true, c = true })
+
+o.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 
 if vim.fn.has("nvim-0.9.0") == 1 then
-	vim.opt.splitkeep = "screen"
-	vim.opt.shortmess:append({ C = true })
+	o.splitkeep = "screen"
+	o.shortmess:append({ C = true })
 end
