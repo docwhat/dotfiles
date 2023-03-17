94b70ff93b275c7b0335a6cb599227cf56cfca4d
Christian Höltje <docwhat@gerf.org>
2023-03-17 17:00:40 -0400

    prettied up the indent lines

--- tag-neovim-next/config/nvim/lua/docwhat/plugins/indent-blankline.lua
+++ tag-neovim-next/config/nvim/lua/docwhat/plugins/indent-blankline.lua
@@ -1,12 +1,16 @@
+-- Rainbow indentations
+-- luacheck: globals vim
 local M = {
   'lukas-reineke/indent-blankline.nvim',
 }
 
 M.opts = {
-  space_char_blankline = ' ',
+  -- https://en.wikipedia.org/wiki/Box-drawing_character is useful
+  -- for finding characters to use.
+  --
+  char = '┆',
+  context_char = '┇',
   show_current_context = true,
-  show_current_context_start = true,
-  show_end_of_line = true,
   char_highlight_list = {
     'IndentBlanklineIndent1',
     'IndentBlanklineIndent2',
@@ -14,18 +18,22 @@ M.opts = {
     'IndentBlanklineIndent4',
     'IndentBlanklineIndent5',
     'IndentBlanklineIndent6',
+    'IndentBlanklineIndent7',
   },
 }
 
 M.config = function(_, opts)
   require('indent_blankline').setup(opts)
 
-  vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+  vim.cmd([[highlight! link IndentBlanklineContextChar Normal]])
+
+  vim.cmd([[highlight! link IndentBlanklineIndent1 Red]])
+  vim.cmd([[highlight! link IndentBlanklineIndent3 Orange]])
+  vim.cmd([[highlight! link IndentBlanklineIndent2 Yellow]])
+  vim.cmd([[highlight! link IndentBlanklineIndent4 Green]])
+  vim.cmd([[highlight! link IndentBlanklineIndent5 Aqua]])
+  vim.cmd([[highlight! link IndentBlanklineIndent6 Blue]])
+  vim.cmd([[highlight! link IndentBlanklineIndent7 Purple]])
 end
 
 return M
