5ea1757e5b2c90204137e883b8ffe3fd975e21b2
Christian Höltje <docwhat@gerf.org>
2023-03-21 14:48:25 -0400

    nvim-next: fixed yamllint wanting keys ordered

--- tag-neovim-next/config/nvim/lua/docwhat/plugins/lsp.lua
+++ tag-neovim-next/config/nvim/lua/docwhat/plugins/lsp.lua
@@ -1,13 +1,22 @@
 -- Description: Language server configuration for Neovim
 -- luacheck: globals vim
 local missingGlobalVimFix = function()
+  local lspZero = require("lsp-zero")
   -- Fix Undefined global 'vim'
-  require("lsp-zero").configure("lua_ls", {
+  lspZero.configure("lua_ls", {
     settings = {
       Lua = {
         diagnostics = {
           globals = { "vim" },
         },
+      },
+    },
+  })
+
+  lspZero.configure("yamlls", {
+    settings = {
+      yaml = {
+        keyOrdering = false,
       },
     },
   })
@@ -60,23 +69,11 @@ local setupLspZero = function()
     nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
     nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-    nmap(
-      "gr",
-      require("telescope.builtin").lsp_references,
-      "[G]oto [R]eferences"
-    )
+    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
     nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
     nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-    nmap(
-      "<leader>ds",
-      require("telescope.builtin").lsp_document_symbols,
-      "[D]ocument [S]ymbols"
-    )
-    nmap(
-      "<leader>ws",
-      require("telescope.builtin").lsp_dynamic_workspace_symbols,
-      "[W]orkspace [S]ymbols"
-    )
+    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
     -- See `:help K` for why this keymap
     nmap("K", vim.lsp.buf.hover, "Hover Documentation")
@@ -84,16 +81,8 @@ local setupLspZero = function()
 
     -- Lesser used LSP functionality
     nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap(
-      "<leader>wa",
-      vim.lsp.buf.add_workspace_folder,
-      "[W]orkspace [A]dd Folder"
-    )
-    nmap(
-      "<leader>wr",
-      vim.lsp.buf.remove_workspace_folder,
-      "[W]orkspace [R]emove Folder"
-    )
+    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
     nmap("<leader>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, "[W]orkspace [L]ist Folders")
