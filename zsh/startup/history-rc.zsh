887b76fb0e9f5a07519684805ebbd3148390eef0
Christian Höltje <docwhat@gerf.org>
2023-09-29 16:24:36 -0400

    feat: (zsh) add histignorespace flag
    I also made the setops match current convention; lowercase, no _ chars

--- zsh/startup/history-rc.zsh
+++ zsh/startup/history-rc.zsh
@@ -5,15 +5,16 @@ HISTFILE=~/.zsh/history
 SAVEHIST=50000
 HISTSIZE=$((SAVEHIST * 1.20))
 
-setopt HIST_BEEP
-setopt EXTENDED_HISTORY
-setopt NO_SHARE_HISTORY
-setopt NO_INC_APPEND_HISTORY
-setopt INC_APPEND_HISTORY_TIME
-setopt HIST_REDUCE_BLANKS
-setopt HIST_IGNORE_DUPS
-setopt HIST_VERIFY
-setopt HIST_FCNTL_LOCK
+setopt extendedhistory
+setopt histbeep
+setopt histfcntllock
+setopt histignoredups
+setopt histignorespace
+setopt histreduceblanks
+setopt histverify
+setopt incappendhistorytime
+setopt noincappendhistory
+setopt nosharehistory
 
 # Don't automatically import new entries in the histfile. Use rehist instead.
 alias rehist='fc -Ri'
