5f96938344591b0bb54aa66a65095c71247e0196
Christian Höltje <docwhat@gerf.org>
2024-03-22 14:07:09 -0400

    use macos path helper to add additional paths

--- tag-macos/zsh/startup/000macos-profile.zsh
+++ tag-macos/zsh/startup/000macos-profile.zsh
@@ -1,4 +1,21 @@
 # macOS has a utility to set up the PATH.
+# I don't want it to clobber _my_ path order, though.
 if [[ -x /usr/libexec/path_helper ]]; then
-  eval `/usr/libexec/path_helper -s`
+  eval "$(/usr/libexec/path_helper -s | sed 's/PATH=/DOCPATH=/g')"
+
+  IFS=: read -r -A docpath <<< "$DOCPATH"
+  for i in "${docpath[@]}"; do
+    if [[ -d "$i" ]]; then
+      path+=("$i")
+    fi
+  done
+
+  IFS=: read -r -A mandocpath <<< "$MANDOCPATH"
+  for i in "${mandocpath[@]}"; do
+    if [[ -d "$i" ]]; then
+      manpath+=("$i")
+    fi
+  done
+
+  typeset -U path manpath
 fi
