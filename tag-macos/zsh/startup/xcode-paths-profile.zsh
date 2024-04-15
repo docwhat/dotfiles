beb1adf3f20d683b0242f0fe514ea0c74ef34180
Christian Höltje <docwhat@gerf.org>
2024-04-15 17:03:40 -0400

    manpath: add SDK man pages on macOS

--- tag-macos/zsh/startup/xcode-paths-profile.zsh
+++ tag-macos/zsh/startup/xcode-paths-profile.zsh
@@ -4,5 +4,8 @@ if [[ -x /usr/bin/xcode-select ]]; then
   if [[ -d "$i" ]]; then
     path+=( "$i/usr/bin" )
     manpath+=( "$i/usr/share/man" )
+    if [[ -d "$i"/SDKs/MacOSX.sdk/usr/share/man ]]; then
+      manpath+=( "$i/SDKs/MacOSX.sdk/usr/share/man" )
+    fi
   fi
 fi
