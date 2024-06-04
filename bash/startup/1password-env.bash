ec85e2a87881036aeb3ab98b72a3470e24a3f7d4
Christian Höltje <docwhat@gerf.org>
2024-06-04 13:53:38 -0400

    lint: add missing brackets

--- bash/startup/1password-env.bash
+++ bash/startup/1password-env.bash
@@ -1,6 +1,6 @@
 # 1Password shell integration.
 
-if [[ -z $SSH_AUTH_SOCK ]] && command -v op 1>/dev/null 2>&1; then
+if [[ -z ${SSH_AUTH_SOCK} ]] && command -v op 1>/dev/null 2>&1; then
   for sock in "${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "${HOME}/.1password/agent.sock"; do
     if [[ -S ${sock} ]]; then
       export SSH_AUTH_SOCK="${sock}"
