fc14e3ad09e7e0ee6e7ef55dba388064abbe5e42
Christian Höltje <docwhat@gerf.org>
2024-07-08 15:44:59 -0400

    fix brew shellenv; only run once

--- zsh/startup/0homebrew-env.zsh
+++ zsh/startup/0homebrew-env.zsh
@@ -1,10 +1,14 @@
 # vim: set ft=bash :
 
 homebrew-env() {
+  if [[ -d "${HOMEBREW_PREFIX}" ]]; then
+    return # We're setup already.
+  fi
+
   local brewbin
   for brewbin in /opt/homebrew/bin/brew /usr/local/bin/brew /home/linuxbrew/.linuxbrew/bin/brew; do
-    if [[ -x "$brewbin" ]]; then
-      eval "$("$brewbin" shellenv zsh)"
+    if [[ -x "${brewbin}" ]]; then
+      eval "$("${brewbin}" shellenv)"
       INFOPATH=${INFOPATH%:}
       MANPATH=${MANPATH%:}
       export INFOPATH MANPATH
