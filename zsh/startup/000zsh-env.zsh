3df3c18abe45463c57bd228d4f3b08aa8b34f866
Christian Höltje <docwhat@gerf.org>
2025-04-09 16:40:39 -0400

    shell: use Library/Caches on macOS
    Since this is the standard location on macOS. This also has the
    advantage of preventing backups, etc. from picking it up.

--- zsh/startup/000zsh-env.zsh
+++ zsh/startup/000zsh-env.zsh
@@ -4,7 +4,13 @@
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
-XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+
+# On MacOS, use the ~/Library/Caches directory.
+if [[ -d "${HOME}/Library/Caches" ]] && [[ "${OSTYPE}" = darwin* ]]; then
+  XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/Library/Caches}"
+else
+  XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+fi
 
 ZSH_CACHE_DIR="${XDG_CACHE_HOME}/zsh"
 
