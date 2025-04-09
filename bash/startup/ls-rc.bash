f4d382b1f542e3abf218d31366fcc9481fd4e8c7
Christian Höltje <docwhat@gerf.org>
2025-04-09 16:42:36 -0400

    bash: add ls aliases as well

--- bash/startup/ls-rc.bash
+++ bash/startup/ls-rc.bash
@@ -1,20 +1,20 @@
-
 # There are different 'ls' versions out there. Let's make them act similar...
+# shellcheck disable=SC2262,SC2263,SC2312
 
-if (( $+commands[eza] )); then
+if command -v eza >/dev/null; then
   alias ls="eza"
   alias ll="eza --long -i --extended"
   alias tree="eza --tree"
-elif (( $+commands[exa] )); then
+elif command -v exa >/dev/null; then
   # EXA is deprecated and unmaintained.
   alias ls="exa --icons --time-style=iso --git --classify --color-scale --color=auto"
   alias ll="ls --long -i --extended"
   alias tree="ls --tree"
-elif (( $+commands[gls] )) && gls --version 2>&1 | grep -q 'coreutils'; then
+elif command -v gls >/dev/null && gls --version 2>&1 | grep -q 'coreutils'; then
   alias gls="gls --color=auto --classify=auto"
   alias ls="gls -C"
   alias ll="gls -al"
-elif ls --color=auto >/dev/null 2>&1 && ls  --classify=auto >/dev/null 2>&1; then
+elif ls --color=auto >/dev/null 2>&1 && ls --classify=auto >/dev/null 2>&1; then
   alias gls="ls --color=auto --classify=auto"
   alias ls="gls -C"
   alias ll="gls -al"
