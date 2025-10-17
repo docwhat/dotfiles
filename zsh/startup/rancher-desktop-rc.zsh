17e55ea2ccaff756421bbcd68c36bf59a10abd23
Christian Höltje <docwhat@gerf.org>
2025-10-17 19:43:18 -0400

    fix extended glob usage

--- zsh/startup/rancher-desktop-rc.zsh
+++ zsh/startup/rancher-desktop-rc.zsh
@@ -1,21 +1,24 @@
 #!zsh
 
-autoload -U is-at-least
+(
+  autoload -U is-at-least
+  setopt extended_glob
 
-if is-at-least 5.2; then
-  if (( ${+commands[nerdctl]} )) && \
-    [[ -n ~/.zsh/functions/_nerdctl(#qN.mh+24) ]]; then
-      {
-        nerdctl completion zsh \
-        > ~/.zsh/functions/_nerdctl
-      } &!
-  fi
+  if is-at-least 5.2; then
+    if (( ${+commands[nerdctl]} )) && \
+      [[ -n ~/.zsh/functions/_nerdctl(#qN.mh+24) ]]; then
+        {
+          nerdctl completion zsh \
+          > ~/.zsh/functions/_nerdctl
+        } &!
+    fi
 
-  if (( ${+commands[rdctl]} )) && \
-    [[ -n ~/.zsh/functions/_rdctl(#qN.mh+24) ]]; then
-      {
-        rdctl completion zsh \
-        > ~/.zsh/functions/_rdctl
-      } &!
+    if (( ${+commands[rdctl]} )) && \
+      [[ -n ~/.zsh/functions/_rdctl(#qN.mh+24) ]]; then
+        {
+          rdctl completion zsh \
+          > ~/.zsh/functions/_rdctl
+        } &!
+    fi
   fi
-fi
+)
