aa670b393ec255204ab81abea4e4e1916ee4af35
Christian Höltje <docwhat@gerf.org>
2026-05-14 14:54:56 -0400

    wezterm: use config builder in host file

--- config/wezterm/docwhat/host.lua
+++ config/wezterm/docwhat/host.lua
@@ -1,3 +1,7 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
 -- This space intentionally left blank.
 -- It is meant to be replaced by rcm host-<hostname> contents.
-return {}
+
+return config
